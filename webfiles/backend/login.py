eb24d7f460b1c6f211db3df4a2b954f507ecfa96
Collin Worth <worthcollin@gmail.com>
2025-03-01 11:25:03 -0800

    starting py file

---
+++ webfiles/backend/login.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+DATABASE_URL = "postgresql://postgres:@localhost:5432/SchemaDB"
+
+try:
+    #connect to postgre server
+    conn = pycopg2.connect(DATABASE_URL)
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT version();")
+    db_version = cursor.ftchone()
+
+    printf(f"Connected to the database")
+
+    cursor.close()
+    conn.close()
+
+except Exception as error:
+    print(f"Error while connecting to postgre server")
+
