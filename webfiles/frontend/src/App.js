e216e81651208f1ee67a21da05f68c06504d322f
Aneesha-Shr <109557810+Aneesha-Shr@users.noreply.github.com>
2025-03-03 20:33:08 -0800

    Create User Page created

--- webfiles/frontend/src/App.js
+++ webfiles/frontend/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Create_Account from "./pages/Create_Account";
 import { Search } from "lucide-react";
 import logo from "./images/logo.png"
 
@@ -26,6 +27,7 @@ function App() {
           {/* Navigation Links */}
           <ul className="nav-links">
             <li><Link to="/login">Login</Link></li>
+            <li><Link to="/create_account">Create Account</Link></li>
             <li> Account </li>
             <li> Cart </li>
           </ul>
@@ -36,6 +38,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/create_account" element={<Create_Account />} />
           </Routes>
         </div>
       </div>
