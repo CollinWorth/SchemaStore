8338b8a123d89833388c935a2dd944abbf00dc86
Collin Worth <worthcollin@gmail.com>
2025-03-03 19:47:17 -0800

    nvm

--- webfiles/frontend/src/pages/Login.js
+++ webfiles/frontend/src/pages/Login.js
@@ -1,57 +1,52 @@
 import React, { useState } from 'react';
 import "./styles/login.css";
-import {loginUser } from "../api";
+import { loginUser } from "../api";
 
 function Login() {
-
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   
-    const handleSubmit = (event) => {
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      console.log('Form submitted');
-      // Handle login logic here (e.g., API call)
-      console.log('Logging in with:', username, password);
-      try {
-        const response = await loginUser(username, password);
-        localStorage.setItem("username", response.username);
-        // This is where you would navigate to a diffrent page becuase successful
-        console.log("login successful from front end");
-      } catch (err) {
-        console.log("Invalid credentials");
-      }
-    };
-
-    
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log('Form submitted');
+    // Handle login logic here (e.g., API call)
+    console.log('Logging in with:', username, password);
+    try {
+      const response = await loginUser(username, password);
+      localStorage.setItem("username", response.username);
+      // This is where you would navigate to a different page because successful
+      console.log("login successful from front end");
+    } catch (err) {
+      console.log("Invalid credentials");
+    }
+  };
   
-    return (
-      <form onSubmit={handleSubmit}>
-        <div className="banner">
-            <h1>Welcome Back! Please Log-In Below:</h1>
-        </div>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="banner">
+        <h1>Welcome Back! Please Log-In Below:</h1>
+      </div>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
 
 export default Login;
