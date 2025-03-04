ccd8ceef42be13bcee609849dedea16a220dae6c
Austin Walker <ajwblueidaho@gmail.com>
2025-03-03 22:09:15 -0800

    I finally got liquibase to work. To get everything setup start from a fresh database

--- webfiles/backend/db-changelogs/categories.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
-
--- changeset austin:1 (create)
-CREATE TABLE categories (
-  category VARCHAR(50) PRIMARY KEY
-);
--- rollback
-DROP TABLE categories;
--- webfiles/backend/db-changelogs/user_roles.sql
+++
@@ -1,6 +0,0 @@
--- liquibase formatted sql
-
--- changeset: austin 1 (create)
-CREATE TABLE user_roles (
-  user_role VARCHAR(50) PRIMARY KEY;
-)
