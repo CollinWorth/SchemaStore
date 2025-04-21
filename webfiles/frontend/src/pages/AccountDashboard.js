95effbfe3e7958ff63f36782b56c9274ffdc388e
Benjamin R Kopf <ben.kopf1371@gmail.com>
2025-04-21 11:12:19 -0700

    Added some features to the account dashboard and now you can log out.

--- webfiles/frontend/src/pages/AccountDashboard.js
+++ webfiles/frontend/src/pages/AccountDashboard.js
@@ -1,9 +1,27 @@
+import "./components/sidebar.css";
+import { useNavigate } from "react-router-dom";
 
 
 function AccountDashboard(){
+    const username = sessionStorage.getItem("username");
+    const navigate = useNavigate();
+
+    const handleLogout = () =>{
+        sessionStorage.removeItem("username");
+        navigate('/');
+    }
+
     return(
         <div>
-            <h1>Welcom To Your Account</h1>
+            <h1>Welcome To Your Account</h1>
+            <div className="sidebar">
+                <h2>Hello {username}</h2>
+                <ul>
+                    <li><a>Section 1</a></li>
+                    <li><a>Section 2</a></li>
+                    <li><a onClick={handleLogout}>Logout</a></li>
+                </ul>
+            </div>                                                
         </div>
     )
 }
