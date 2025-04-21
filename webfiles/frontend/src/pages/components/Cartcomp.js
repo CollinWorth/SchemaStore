b6d677d7fc85aef5d8802f7e6b117b55f9e2ee15
Benjamin R Kopf <ben.kopf1371@gmail.com>
2025-04-21 11:12:19 -0700

    Added some features to the account dashboard and now you can log out.

--- webfiles/frontend/src/pages/components/Cartcomp.js
+++ webfiles/frontend/src/pages/components/Cartcomp.js
@@ -1,18 +1,19 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const CartContext = createContext();
 
 export const useCart = () => useContext(CartContext);
 
 export const CartProvider = ({ children }) => {
-  const username = localStorage.getItem("username");
+  const username = sessionStorage.getItem("username");
 
   const [cart, setCart] = useState({}); // { sku: quantity }
 
 
   if (username === "undefined") {
-    console.warn("No username in localStorage — cart requests may fail");
+    console.warn("No username in sessionStorage — cart requests may fail");
     return;
   }
 
