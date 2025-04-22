5a6708d3321fde3dc91de2139f0dd84059eb0e95
Austin Walker <ajwblueidaho@outlook.com>
2025-04-22 10:07:11 -0700

    Chnaged img in products to text

--- webfiles/backend/db-changelogs/iterations/u.sql
+++ webfiles/backend/db-changelogs/iterations/u.sql
@@ -5,3 +5,10 @@ BEGIN;
 ALTER TABLE orders
    ADD COLUMN created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
 COMMIT;
+
+-- changeset austin:2
+-- change img in products to text type
+BEGIN;
+ALTER TABLE products
+   ALTER COLUMN img TYPE TEXT;
+COMMIT;
