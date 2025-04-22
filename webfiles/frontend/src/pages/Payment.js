7bc309afdc02e88e6ab36992280ae77fa6764f08
Aneesha-Shr <109557810+Aneesha-Shr@users.noreply.github.com>
2025-04-22 00:12:32 -0700

    Update Payment.js

--- webfiles/frontend/src/pages/Payment.js
+++ webfiles/frontend/src/pages/Payment.js
@@ -7,8 +7,11 @@ import { useNavigate } from 'react-router-dom';
 function Payment(){
     return(
         <div className='cart-page'>
-            <h2>Payment</h2>
-            <p>Thank you for your purchase!</p>
+            <h2>Thank you for your purchase!</h2>
+            <p>Your items should arrive in 100 years, we appreciate your business.</p>
+            <p>We will send you an email when your items are shipped.</p>
+            <p>Have a great day!</p>
+            <p>Click <a href="/">here</a> to return to the home page.</p>
         </div>
     )
 } export default Payment;
