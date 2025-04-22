3d2f286d12c742d29bb92492c22835c7b232bfe6
Austin Walker <ajwblueidaho@gmail.com>
2025-04-21 18:46:34 -0700

    Fixed a liquibase file

--- webfiles/backend/db-changelogs/iterations/three.sql
+++ webfiles/backend/db-changelogs/iterations/three.sql
@@ -1,12 +1,33 @@
 
 -- changeset austin:1
 -- add user roles
+BEGIN;
 INSERT INTO roles(user_role) VALUES
    ('admin'),
    ('buyer'),
-   ('seller')
+   ('seller');
+COMMIT;
 
 -- changeset austin:2
 -- add first admin user
-INSERT INTO users (username, password, email, first_name, last_name, role_id)
-   VALUES ('admin', 'tail', 'admin@idontexist.com', 'Happy', 'Ghast', 1);
+BEGIN;
+INSERT INTO users (username, password, email, role_id)
+   VALUES ('admin', 'tail', 'admin@idontexist.com', 1);
+COMMIT;
+
+-- changeset austin:3
+-- add order number to orders table
+BEGIN;
+ALTER TABLE orders
+   DROP CONSTRAINT product_orders_pkey,
+   DROP COLUMN product_sku,
+   DROP COLUMN amount,
+   ADD COLUMN id SERIAL PRIMARY KEY;
+
+CREATE TABLE ordered_items (
+   order_id INT NOT NULL,
+   product_sku TEXT NOT NULL,
+   amount INT NOT NULL,
+   FOREIGN KEY (order_id) REFERENCES orders(id)
+);
+COMMIT;
